13e5328bc694c3dc52e460ddb332e621ae80770c
Rowan Rasmusson <144362157+rrasmuss4200@users.noreply.github.com>
2025-10-15 14:34:50 -0600

    Update countdown_timer_fsm_tb.vhd oct 15

--- lab2/countdown_timer_fsm_tb.vhd
+++ lab2/countdown_timer_fsm_tb.vhd
@@ -38,7 +38,7 @@ end countdown_timer_fsm_tb;
 architecture Behavioral of countdown_timer_fsm_tb is
     SIGNAL switches : std_logic_vector(3 downto 0) := "1010";
     SIGNAL load_btn : std_logic := '0';
-    SIGNAL clk: std_logic := '1';
+--    SIGNAL clk: std_logic := '1';
     SIGNAL reset: std_logic := '0';
     SIGNAL start_btn: std_logic := '0'; 
     SIGNAL clk_en_1hz: std_logic := '1';
@@ -47,7 +47,6 @@ begin
     DUT : entity work.countdown_timer_fsm(Behavioral) PORT MAP (
         switches=>switches,
         load_btn => load_btn,
-        clk => clk,
         reset => reset,
         start_btn => start_btn,
         clk_en_1hz => clk_en_1hz,
@@ -65,32 +64,33 @@ begin
         wait for 250ns;
         
     -- Reset Tests
---        -- Reset durring default run (ie. 10s)
---        start_btn <= '1';
---        wait for 10ns;
---        start_btn <= '0';
---        wait for 50ns;
---        reset <= '1';
---        wait for 10ns;
---        reset <= '0';
+        -- Reset durring default run (ie. 10s)
+        start_btn <= '1';
+        wait for 10ns;
+        start_btn <= '0';
+        wait for 50ns;
+        reset <= '1';
+        wait for 10ns;
+        reset <= '0';
         
---        -- Reset when in idle
---        wait for 50ns;
---        reset <= '1';
---        wait for 10ns;
---        reset <= '0';
+        -- Reset when in idle
+        wait for 50ns;
+        reset <= '1';
+        wait for 10ns;
+        reset <= '0';
              
-    -- Load Test
---        -- Load 15s
---        switches <= "1111";
---        wait for 10ns;
---        load_btn <= '1';
---        wait for 10ns;
---        load_btn <= '0';
---        start_btn <= '1';
---        wait for 10ns;
---        start_btn <= '0';
---        wait for 350ns;
+--     Load Test
+        -- Load 15s
+        wait for 10ns;
+        switches <= "1111";
+        wait for 10ns;
+        load_btn <= '1';
+        wait for 10ns;
+        load_btn <= '0';
+        start_btn <= '1';
+        wait for 10ns;
+        start_btn <= '0';
+        wait for 350ns;
 
         -- Load 5s
         switches <= "0101";
@@ -126,15 +126,15 @@ begin
     end process stimulus;
     
     -- Process for our TB clock
-    clk_proc : process
-    begin
-        loop
-            wait for 1ns;
-            clk <= '0';
-            wait for 1ns;
-            clk <= '1';
-        end loop;
-    end process;
+--    clk_proc : process
+--    begin
+--        loop
+--            wait for 1ns;
+--            clk <= '0';
+--            wait for 1ns;
+--            clk <= '1';
+--        end loop;
+--    end process;
     
     one_hz_clock : process
     begin
