75efa8ad90904057a473b5ea3fa27ed541631642
Rowan Rasmusson <144362157+rrasmuss4200@users.noreply.github.com>
2025-10-22 16:00:59 -0600

    Update part_3_secure_elem_fsm.vhdl working

--- lab2/part_3/part_3_secure_elem_fsm.vhdl
+++ lab2/part_3/part_3_secure_elem_fsm.vhdl
@@ -1,10 +1,10 @@
 ----------------------------------------------------------------------------------
 -- Company: 
--- Engineer: Yaaqoob, Rowan
+-- Engineer: 
 -- 
--- Create Date: 10/08/2025 01:14:29 PM
+-- Create Date: 10/22/2025 01:56:40 PM
 -- Design Name: 
--- Module Name: countdown_timer_fsm - Behavioral
+-- Module Name: secure_element_fsm - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -21,7 +21,6 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.numeric_std.all;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -32,105 +31,91 @@ use IEEE.numeric_std.all;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity secure_elem_fsm is
-  Port ( 
-    reset: in std_logic;
-    switches: in std_logic_vector(3 downto 0);
-    start_btn: in std_logic;
-    load_btn: in std_logic;
-    
-    clk_en_1hz: in std_logic;
-    
-    count_out: out std_logic_vector(3 downto 0);
-    rgb_state: out std_logic_vector(2 downto 0)
-  );
-end secure_elem_fsm;
+entity secure_element_fsm is
+  Port (
+    clk : in std_logic;
+    busy : in std_logic := '1'; -- so we start in startup and not alarm
+    self_test : in std_logic;
+    startup : in std_logic;
+    sleep : in std_logic;
+    request : in std_logic;
+    secure_channel : in std_logic;
+    attack_detected : in std_logic;
+    reset : in std_logic;
+    RGB : out std_logic_vector(2 downto 0);
+   );
+end secure_element_fsm;
 
-
-architecture Behavioral of secure_elem_fsm is
-
-    type state_type is (IDLE, LOAD, COUNTDOWN, DONE);
-    signal curr_state : state_type := IDLE;
-    signal next_state : state_type;
-    constant DEFAULT_COUNT : INTEGER := 10;
-    signal curr_count_reg : INTEGER := 0; -- Start counter at 0 in IDLE
-    signal loaded_val : INTEGER := DEFAULT_COUNT;
-    
-    -- Next-value signals for registers
-    signal loaded_val_next : INTEGER;
-    signal curr_count_regnext : INTEGER;
-    
+architecture Behavioral of secure_element_fsm is
+    type statetype is (ST_STARTUP, ST_SECURE_CHANNEL, ST_IDLE, ST_SLEEP, ST_ALARM);
+    signal present_state, next_state : statetype := ST_STARTUP;
 begin
-    -- PROCESS 1: Sequential Logic (State Updates)
-    -- This process is clocked by the 1Hz signal and updates all our registers on its rising edge.
-    process (clk_en_1hz, reset) is 
+
+    controller_state_reg : process (clk) is
     begin
         if reset = '1' then
-            curr_state <= IDLE;
-            curr_count_reg <= 0;
-            loaded_val <= DEFAULT_COUNT;
-        elsif rising_edge(clk_en_1hz) then
-            curr_state <= next_state;
-            curr_count_reg <= curr_count_regnext;
-            loaded_val <= loaded_val_next;
+            present_state <= ST_STARTUP;
+        elsif rising_edge(clk) then
+            present_state <= next_state;
         end if;
-    end process;
-    
-    -- PROCESS 2: Combinational Logic (Next-State and Output Logic)
-    -- Notes: purely combinational.
-    -- It calculates the next state and next register values based on the current state and inputs.
-    process (curr_state, loaded_val, curr_count_reg, load_btn, start_btn, switches) is
+    end process controller_state_reg;
+
+-- when exiting start up state, check BUSY, THEN self-test
+
+
+    state_combinational_logic : process (present_state, busy, self_test, startup, sleep, request, secure_channel, attack_detected) is
     begin
-        -- next_state <= curr_state;
-        -- loaded_val_next <= loaded_val;
-        -- curr_count_regnext <= curr_count_reg;
+--        next_state <= present_state;
+        case present_state is
+            when ST_STARTUP => 
+                RGB <= "001"; -- blue
+                if busy = '0' and self_test = '1' then
+                     next_state <= ST_IDLE;
+                elsif busy = '0' and self_test = '0' then
+                    next_state <= ST_ALARM;
+                else
+                    next_state <= ST_STARTUP;
+                end if;
 
-        case curr_state is
-            when IDLE =>
-                -- rgb_state <= "100"; -- Red
-                -- count_out <= std_logic_vector(to_unsigned(loaded_val, 4));
+            when ST_SECURE_CHANNEL => 
+                RGB <= "010"; -- green
+                if secure_channel = '0' then
+                    next_state <= ST_IDLE;
+                elsif sleep = '1' then
+                    next_state <= ST_SLEEP;
+                else 
+                    next_state <= ST_SECURE_CHANNEL;
+                end if;
                 
-                -- if load_btn = '1' then
-                --     next_state <= LOAD;
-                -- elsif start_btn = '1' then
-                --     next_state <= COUNTDOWN;
-                --     -- NOTE: preload the counter with the LAST loaded value
-                --     curr_count_regnext <= loaded_val; 
-                -- end if;
-            
-            when LOAD =>
-                -- rgb_state <= "010"; -- Green
-            
-                -- --First check if switches are valid ie. (0-15) if invalid (when others) load 10 then send back to IDLE state
-                -- if to_integer(unsigned(switches)) > 0 and to_integer(unsigned(switches)) < 16 then
-                --      loaded_val_next <= to_integer(unsigned(switches));
-                -- else
-                --      loaded_val_next <= DEFAULT_COUNT;
-                -- end if;
+            when ST_IDLE => 
+                RGB <= "110"; -- yellow
+                if attack_detected = '1' then
+                    next_state <= ST_ALARM;
+                elsif secure_channel = '1' then
+                    next_state <= ST_SECURE_CHANNEL;
+                elsif startup = '1' then
+                    next_state <= ST_STARTUP;
+                elsif sleep = '1' then
+                    next_state <= ST_SLEEP;
+                else
+                    next_state <= ST_IDLE;
+                end if;
                 
-                -- count_out <= std_logic_vector(to_unsigned(loaded_val_next, 4));
-                
-                -- -- Design choice: Make LOAD a transient state that AUTO returns to IDLE.
-                -- next_state <= IDLE;
+            when ST_SLEEP =>
+                RGB <= "111"; -- white
+                if request = '1' then
+                    next_state <= ST_IDLE;
+                else
+                    next_state <= ST_SLEEP;
+                end if;
 
-            when COUNTDOWN =>
-                -- rgb_state <= "001"; -- Blue
-                -- count_out <= std_logic_vector(to_unsigned(curr_count_reg, 4));
-                
-                -- if curr_count_reg > 0 then
-                --     curr_count_regnext <= curr_count_reg - 1;
-                -- else -- curr_count_reg is 0
-                --     next_state <= DONE;
-                -- end if;
-            
-            when DONE =>
-                -- rgb_state <= "011"; -- Cyan
-                -- count_out <= std_logic_vector(to_unsigned(curr_count_reg, 4)); -- Displays 0
-                
-                -- -- AUTO go back to IDLE on the next clock tick
-                -- next_state <= IDLE;
-
+            when ST_ALARM =>
+                RGB <= "100";
+                next_state <= ST_ALARM;
         end case;
-    end process;
+    end process state_combinational_logic;
+
+
+
 
 end Behavioral;
