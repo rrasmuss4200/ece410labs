0bdd1bacba163e3159194c9cdcbc97c24fdb0532
Rowan Rasmusson <144362157+rrasmuss4200@users.noreply.github.com>
2025-11-28 15:53:35 -0700

    Part 3 DONE

--- Lab3/lab3_cpu/multi_cycle_riscv_cpu/combined_memory.vhdl
+++ Lab3/lab3_cpu/multi_cycle_riscv_cpu/combined_memory.vhdl
@@ -54,17 +54,67 @@ ARCHITECTURE rtl OF combined_mem IS
         -- 0x00145413   srli x8, x8, 1
         28 => x"13", 29 => x"54", 30 => x"14", 31 => x"00",
 
-        -- 0xFE744AE3 blt x8, x7, -12 (x8 < x7)
-        32 => x"E3", 33 => x"4A", 34 => x"74", 35 => x"FE",
+--        -- 0xFE744AE3 blt x8, x7, -12 (x8 < x7)
+--        32 => x"E3", 33 => x"4A", 34 => x"74", 35 => x"FE",
 
-        -- 0xFE644AE3 blt x8, x6, -12 (x8 < x6)
-        36 => x"E3", 37 => x"4A", 38 => x"64", 39 => x"FE",
-
-        -- 0x0000000B HALT
-        40 => x"0B", 41 => x"00", 42 => x"00", 43 => x"00",
-
-
+--        -- 0xFE644AE3 blt x8, x6, -12 (x8 < x6)
+--        36 => x"E3", 37 => x"4A", 38 => x"64", 39 => x"FE",
         
+        -- x7 = 0  --> Holds our count
+        -- x8 = 1  --> Holds a value of 1 (to increment)
+        -- x2 = 610433 ---> Holds our test case number
+        -- x3       ---> result of ANDi
+        -- x4 = 0  --> CONSTANT holds our zero value
+        
+--        Reg x2 contains -> (610433)16 = (0110 0001 0000 0100 0011 0011)2
+        -- we expect an answer of 8, thus x7 = 0x00000008
+          -- 0. Initialize x3 to be 0x0000033 this is becasue we ran out of simulation time since each inst. is 3clk cycles
+          -- 1. and with 0x1 and store in x3
+          -- 2. a) if result (x3) is 1 -> do not branch ie. continue increment our count
+--              b) if result (x3) is 0 -> branch to skip ie. do not increment
+          -- 3. incremnt the count if (a) by addint reg x8 which holds 1 to x7 which holds our count
+          -- 4. Shift the reg x2 which has our test case number
+          -- 5. a) if we have shifted all the bits (ie. the end) --> branch to Hault
+--              b) if not continue
+--           6. a) if we are not done shifting all bits (!= 0) ---> go back to step 1
+        
+        --init: 
+    --        andi x2, x2, 51
+        --loop:
+        --    andi  x3, x2, 1          # check LSB
+        --    beq   x3, x4, 8
+        --    add   x7, x7, x8          # count++ note -> x8 holds '1'
+        --skip:
+        --    srli  x2, x2, 1
+        --    beq   x2, x4, 8
+        --    beq   x3, x3, -20   # unconditional jump
+        --done:
+            -- HALT
+
+        -- 0x03317113 andi x2, x2, 51
+        32 => x"13", 33 => x"71", 34 => x"31", 35 => x"03",
+
+        -- 0x00117193 andi x3, x2, 1
+        36 => x"93", 37 => x"71", 38 => x"11", 39 => x"00",
+
+        -- 0x00418463  beq x3, x4, 8
+        40 => x"63", 41 => x"84", 42 => x"41", 43 => x"00",
+
+        -- 0x008383B3  add   x7, x7, x8
+        44 => x"B3", 45 => x"83", 46 => x"83", 47 => x"00",
+        
+        -- 0x00115113  srli  x2, x2, 1
+        48 => x"13", 49 => x"51", 50 => x"11", 51 => x"00",
+        
+        -- 0x00410463  beq   x2, x4, 8
+        52 => x"63", 53 => x"04", 54 => x"41", 55 => x"00",
+        
+        -- 0xFE3186E3  beq   x3, x3, -20
+        56 => x"E3", 57 => x"86", 58 => x"31", 59 => x"FE",
+        
+        -- 0x0000000B HALT
+        60 => x"0B", 61 => x"00", 62 => x"00", 63 => x"00",
+
         OTHERS => (OTHERS => '0')
     );
 
